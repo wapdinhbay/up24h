0de4191082346deed55293e24104b3c76237aed6
wapdinhbay <42421171+wapdinhbay@users.noreply.github.com>
2019-01-01 16:16:39 +0700

    Add files via upload

---
+++ upfile/set.php
@@ -0,0 +1,10 @@
+<?php
+$auto = 'http://xtgem.com/autologin/ae1b90bed0d5ce2411a0f23461eede90/dXAyNGgubXcubHQ=';
+$tenwap = explode('/',$auto);
+$server = $tenwap[2];
+$tenwap = base64_decode($tenwap[5]);
+$ua = 'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54';
+$cookies = '';
+$wap4 = 'http://up24h.viwap.com';
+$typee = array('jpeg','jpg','png','gif','mp3','mp4','avi','3gp','exe','zip','rar','js','css','jar','jad','apk','pdf');
+?>
